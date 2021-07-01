c578583b2a6572771c45e4286d63802d2e1cb945
CMierez <carlos.mierez20@gmail.com>
2021-07-01 17:19:10 -0300

    Heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#69ah*6gmazb)e(%ie0&6gs5gl$c)&x!i@wp=)c_=z)i9n-)&d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
