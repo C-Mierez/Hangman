2755f7594d07da4313c735a58a097ee25cb9e42d
CMierez <carlos.mierez20@gmail.com>
2021-07-01 14:28:35 -0300

    Heroku deployment

--- core/asgi.py
+++ core/asgi.py
@@ -10,17 +10,7 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-
-from game import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-# application = get_asgi_application()
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(routing.ws_urlpatterns)
-    ),
-})
+application = get_asgi_application()
