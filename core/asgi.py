f8f8f083c9f56ba1f64e1d1b30e853034acb89a4
CMierez <carlos.mierez20@gmail.com>
2021-07-01 13:33:09 -0300

    Heroku deployment

--- core/asgi.py
+++ core/asgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels import asgi
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
+channel_layer = asgi.get_channel_layer()
